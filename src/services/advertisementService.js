ba1f1f273662c7a56c649dc54ea89808e5012e0b
Yusuf Mücahit Solmaz <83172478+Yusuf-Solmaz@users.noreply.github.com>
2021-06-17 17:21:55 +0300

    Delete src directory

--- src/services/advertisementService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export default class advertisementService{
-    getAdvertisements(){
-        return axios.get("http://localhost:8080/api/jobadvertisements/getall")
-    }
-    
-}
