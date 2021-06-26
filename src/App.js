04920cbb7714cbfe40328da04e42080299864fd9
Yusuf Mücahit Solmaz <83172478+Yusuf-Solmaz@users.noreply.github.com>
2021-06-26 13:17:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Navi from './layouts/Navi';
 import { Container } from 'semantic-ui-react';
-import PostAdv from './pages/PostAdv';
-import ConfirmJobAd from './pages/ConfirmJobAd.jsx';
 
+import Dashboard from './layouts/Dashboard';
 
 
 function App(){
   return (
     <div className="App">
-      <Navi/>
+      <Navi />
       <Container className="main">
-      {/* <Dashboard/> */}
-       <PostAdv/> 
-      {/* <ConfirmJobAd/> */}
-
+        <Dashboard />
       </Container>
-      
     </div>
   );
 }
